2165f2374cb34dfa4ab114b953f90720b0eea1ae
medinazorin_desk <medinazorin@gmail.com>
2022-03-02 20:04:12 +0100

    advances in code logic

--- testing/iot_time_calculation.py
+++ testing/iot_time_calculation.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 
 from google.cloud import bigquery
 import pandas as pd
@@ -11,11 +12,9 @@ client = bigquery.Client()
 job_config = bigquery.QueryJobConfig(destination=TABLE)
 
 df_input = pd.read_csv("testing/testing-iot.csv").to_dict('records')
-print(df_input)
 
 def upload_status(data):
-    print('TO UPLOAD')
-    print(data)
+
     data = [{'parking_id':data['parking_id'],'arrival_time':data['timeStamp'],'departure_time':None,'total_time':None}]
     bq_client = bigquery.Client()
     table = bq_client.get_table("{}.{}.{}".format("dp2-test-342416", "edemDataset", "status")) # TODO: Change to unified table variable
@@ -23,44 +22,57 @@ def upload_status(data):
     if errors == []:
         print("success")
 
-def update_status(data, previous_data):
+def update_status(data):
+    print('DATA TO UPDATE')
     print(data)
-    exit(0)
-    errors = client.insert_rows_json(TABLE, data)  # Make an API request.
-    if errors == []:
-        print("New rows have been added.")
+    query = f"UPDATE '{TABLE}' SET " \
+    f"departure_time = '{data['departure_time']}', "\
+    f"total_time = '{data['total_time']}' " \
+    f"WHERE parking_id = '{data['parking_id']}' AND arrival_time = '{data['arrival_time']}'"
+    print(query)
+    # query_job = client.query((dml_statement))  # API request
+    # query_job.result()  # Waits for statement to finish
+    client.query(query)
+
+def process_true(current_status, previous_status):
+    if not previous_status:
+        print('BQ VACIO')
+        upload_status(current_status)
     else:
-        print("Encountered errors while inserting rows: {}".format(errors))
+        if previous_status[0]['departure_time'] != None:
+            upload_status(current_status)
 
-def process_false(current_status, previus_status):
-    if previus_status and previus_status['departure_time']!=None:
+def process_false(current_status, previous_status):
+    print('ENTRO A PRERPOCESS',previous_status)
+    if not previous_status:
+        print('BQ VACIO')
+    elif previous_status[0]['departure_time']==None:
+        previous_status = previous_status[0]
+        date_format_str = '%Y-%m-%d %H:%M:%S.%f'
+        start_timestamp = datetime.strptime(previous_status['arrival_time'], date_format_str)
+        end_timestamp = datetime.strptime(current_status['timeStamp'], date_format_str)
+        diff = end_timestamp - start_timestamp
+        print(diff)
+        status_update = {'parking_id':previous_status['parking_id'],'arrival_time':previous_status['arrival_time'],'departure_time':current_status['timeStamp'],'total_time':str(diff)}
+        update_status(status_update)
 
-        start_timestamp = previus_status['arrival_time']
-        end_timestamp = current_status['timeStamp']
-        print(start_timestamp,end_timestamp)
-        print(type(start_timestamp),type(end_timestamp))
-        exit(0)
-
-for current_status in df_input:
+for c,current_status in enumerate(df_input):
 
     """ READ BIGQUERY STATUS"""
-    # q = """SELECT * FROM `dp2-test-342416.edemDataset.status` where parking_id = 'parking1' order by timeStamp desc Limit 1;"""
     q = f"""SELECT * FROM {TABLE} where parking_id = '{current_status['parking_id']}' order by arrival_time desc Limit 1;"""
-    print(q)
 
-    previus_status = (
+    previous_status = (
         client.query(q)
         .result()
         .to_dataframe()).to_dict('records')
-    print('#~#~#~#~#~#~#~#~#~#~# OJITO CON LO QUE TENEMOS #~#~#~#~#~#~#~#~#~#~#')
-    print(previus_status, current_status)
-    print(type(previus_status), current_status)
+    print(f'#~#~#~#~#~#~#~#~#~#~# {c} #~#~#~#~#~#~#~#~#~#~#')
+    # print(previous_status, current_status)
 
     if current_status['ocupado']:
         print('OCUPADO')
-        upload_status(current_status)
+        process_true(current_status, previous_status)
     else:
         print('DESOCUPADO')
-        process_false(current_status, previus_status)
+        process_false(current_status, previous_status)
 
     time.sleep(2) # TODO: quitar
