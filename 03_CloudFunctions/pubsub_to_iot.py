f02ee124f6125d0f2dbd78df0cf6e7d7be0cfe28
Cristian <medinazorin@gmail.com>
2022-03-05 20:27:39 +0100

    cloud_upd

--- 03_CloudFunctions/pubsub_to_iot.py
+++ 03_CloudFunctions/pubsub_to_iot.py
@@ -3,24 +3,21 @@
 
 # Import libraries
 import base64, json, sys, os
-from google.cloud import logging as cloudlogging
+import google.cloud.logging
 import logging
 
 # Read from PubSub
-def pubsub_to_iot(event, context):
+def calculate_time(event, context):
     # Read message from Pubsub (decode from Base64)
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
 
     # Load json
     message = json.loads(pubsub_message)
 
-    lg_client = cloudlogging.Client()
+    client = google.cloud.logging.Client()
+    client.setup_logging()
 
-    lg_handler = lg_client.get_default_handler()
-    cloud_logger = logging.getLogger("cloudLogger")
-    cloud_logger.setLevel(logging.INFO)
-    cloud_logger.addHandler(lg_handler)
-    cloud_logger.info('ME LLEGA MENSAJE EN LA FUNCION CLOUD: ', message)
+    logging.info('ME LLEGA MENSAJE EN LA FUNCION CLOUD: ', message)
 
     if message['status'] == 'salida':
-        cloud_logger.info('SALIDA')
+        logging.info('SALIDA')
