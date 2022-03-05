13b30dadc5c40d70b3c1e962b747123bfe587ca7
medinazorin_desk <medinazorin@gmail.com>
2022-03-05 19:02:12 +0100

    important change to dataflow update

--- 02_Dataflow/DataflowCode.py
+++ 02_Dataflow/DataflowCode.py
@@ -95,7 +95,7 @@ def edemData(output_table, project_id):
 
         #(data
         #    | "Filter messages" >> beam.Filter(lambda element: element['status'] == 'salida')
-        #    | "WriteToPubSub" >> beam.io.WriteToPubSub(topic=f"projects/{project_id}/topics/iotToCloudFunctions",
+        #    | "WriteToPubSub" >> beam.io.WriteToPubSub(topic=f"projects/{project_id}/topics/IotToCloudFunctions",
         #                                                with_attributes=False)
         #)
 
